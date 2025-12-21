26b55f02ff1807db7d20daa05f1c4cb4c438899c
Anand Patil <96280518+apatilgtn@users.noreply.github.com>
2025-12-21 16:47:58 +1100

    Delete Downloads directory

--- Downloads/UAL-main/UAL-main/ual-framework/index.ts
+++
@@ -1,9 +0,0 @@
-// Export all core interfaces and classes
-export * from './core/interfaces';
-export * from './core/base-connector';
-export * from './core/registry';
-
-// Export connectors
-export * from './connectors/database-connector';
-export * from './connectors/api-connector';
-export * from './connectors/file-connector';
